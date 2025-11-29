6f605bea1df1ab886c8b1ba5254cf0777556fa4a
Егор <42337331+tsentavta@users.noreply.github.com>
2025-11-29 22:54:49 +0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,6 @@
-using namespace std
-print('hello world')
+import time
+def console_greeting():  
+    name = input("Введите ваше имя: ")  
+    print(f"Hello world from {name}")
+    time.sleep(10)
+console_greeting()
