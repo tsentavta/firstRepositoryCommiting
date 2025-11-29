ae59c4cea01a35508b141c4dc5744af0689516b8
Егор <42337331+tsentavta@users.noreply.github.com>
2025-11-29 19:46:54 +0000

    комментарий в коде

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,5 @@
 import time
 def console_greeting():  
-    name = input("Введите ваше имя: ")  
-    print(f"Hello world from {name}")
+    name = input("Введите ваше имя: ")  # запрос в терминале
+    print(f"Hello world from {name}") 
 console_greeting()
