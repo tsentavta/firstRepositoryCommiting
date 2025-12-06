e6079cad2416ac8744a3b8fb55b9d040175dca30
Егор <42337331+tsentavta@users.noreply.github.com>
2025-12-06 11:02:30 +0400

    перевод комментария

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
 # в новую ветку 
 import time
 def console_greeting():  
-    name = input("Введите ваше имя: ")  # запрос в терминале локально 
+    name = input("Введите ваше имя: ")  # reqest location comments
     print(f"Hello world from {name}") 
 console_greeting()
